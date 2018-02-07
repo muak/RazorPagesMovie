4ccc8b2fd4b9a0fef4283b3126f9678acf6dd878
kamu <kamu@kamunoMacBook-Pro.local>
2018-02-08 00:12:36 +0900

    Search by Genre

--- RazorPagesmovie/Pages/Movies/Edit.cshtml.cs
+++ RazorPagesmovie/Pages/Movies/Edit.cshtml.cs
@@ -53,7 +53,7 @@ namespace RazorPagesmovie.Pages.Movies
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!MovieExists(Movie.ID))
+                if (!_context.Movie.Any(e => e.ID == Movie.ID))
                 {
                     return NotFound();
                 }
--- RazorPagesmovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesmovie/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -19,10 +20,25 @@ namespace RazorPagesmovie.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
+        public SelectList Genres;
+        public string MovieGenre { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string movieGenre,string searchString)
         {
-            Movie = await _context.Movie.ToListAsync();
+            var genreQuery = _context.Movie.OrderBy(x => x.Genre).Select(x => x.Genre);
+
+            var movies = _context.Movie.Select(x=>x);
+
+            if(!string.IsNullOrEmpty(searchString)){
+                movies = movies.Where(x => x.Title.Contains(searchString));
+            }
+
+            if(!string.IsNullOrEmpty(movieGenre)){
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
